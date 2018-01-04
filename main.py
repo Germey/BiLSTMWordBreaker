64ca5f7de356a5f8bf23d87251a22b11afb8432f
Germey <cqc@cuiqingcai.com>
2018-01-05 00:46:28 +0800

    init

---
+++ main.py
@@ -0,0 +1,168 @@
+import argparse
+import tensorflow as tf
+import pickle
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+FLAGS = None
+
+
+def load_data():
+    """
+    Load data from pickle
+    :return: Arrays
+    """
+    with open(FLAGS.source_data, 'rb') as f:
+        data_x = pickle.load(f)
+        data_y = pickle.load(f)
+        word2id = pickle.load(f)
+        id2word = pickle.load(f)
+        tag2id = pickle.load(f)
+        id2tag = pickle.load(f)
+        return data_x, data_y, word2id, id2word, tag2id, id2tag
+
+
+def get_data(data_x, data_y):
+    """
+    Split data from loaded data
+    :param data_x:
+    :param data_y:
+    :return: Arrays
+    """
+    print('Data X Length', len(data_x), 'Data Y Length', len(data_y))
+    print('Data X Example', data_x[0])
+    print('Data Y Example', data_y[0])
+    
+    train_x, test_x, train_y, test_y = train_test_split(data_x, data_y, test_size=0.2, random_state=40)
+    train_x, valid_x, train_y, valid_y = train_test_split(train_x, train_y, test_size=0.2, random_state=40)
+    
+    print('Train X Shape', train_x.shape, 'Train Y Shape', train_y.shape)
+    print('Valid X Shape', valid_x.shape, 'Valid Y Shape', valid_x.shape)
+    print('Test Y Shape', test_x.shape, 'Test Y Shape', test_y.shape)
+    return train_x, train_y, valid_x, valid_y, test_x, test_y
+
+
+def get_generator(data_x, data_y, batch_size):
+    """
+    Get generator of x, y data
+    :param data_x:
+    :param data_y:
+    :return: Iterator
+    """
+    dataset = tf.data.Dataset.from_tensor_slices((data_x, data_y))
+    dataset = dataset.batch(batch_size)
+    iterator = dataset.make_initializable_iterator()
+    return iterator
+
+
+def weight(shape, stddev=0.1, mean=0):
+    initial = tf.truncated_normal(shape=shape, mean=mean, stddev=stddev)
+    return tf.Variable(initial)
+
+
+def bias(shape, value=0.1):
+    initial = tf.constant(value=value, shape=shape)
+    return tf.Variable(initial)
+
+
+def lstm_cell(num_units, keep_prob=0.5):
+    cell = tf.nn.rnn_cell.LSTMCell(num_units, reuse=tf.AUTO_REUSE)
+    return tf.nn.rnn_cell.DropoutWrapper(cell, output_keep_prob=keep_prob)
+
+
+def main():
+    # Load data
+    data_x, data_y, word2id, id2word, tag2id, id2tag = load_data()
+    # Split data
+    train_x, train_y, valid_x, valid_y, test_x, test_y = get_data(data_x, data_y)
+    
+    vocab_size = len(word2id)
+    print('Vocab Size', vocab_size)
+    
+    # Input Layer
+    with tf.variable_scope('inputs'):
+        x = tf.placeholder(tf.int32, [None, FLAGS.time_step], name='x_input')
+        y_label = tf.placeholder(tf.int32, [None, FLAGS.time_step], name='y_label')
+    
+    # Embedding Layer
+    with tf.variable_scope('embedding'):
+        embedding = tf.Variable(tf.random_normal([vocab_size, FLAGS.embedding_size]), dtype=tf.float32)
+    inputs = tf.nn.embedding_lookup(embedding, x)
+    
+    # Variables
+    batch_size = tf.placeholder(tf.int32, [])
+    keep_prob = tf.placeholder(tf.float32, [])
+    
+    # RNN Layer
+    cell_fw = tf.nn.rnn_cell.MultiRNNCell([lstm_cell(FLAGS.num_units, keep_prob) for _ in range(FLAGS.num_layer)])
+    cell_bw = tf.nn.rnn_cell.MultiRNNCell([lstm_cell(FLAGS.num_units, keep_prob) for _ in range(FLAGS.num_layer)])
+    initial_state_fw = cell_fw.zero_state(batch_size, tf.float32)
+    initial_state_bw = cell_bw.zero_state(batch_size, tf.float32)
+    output_fw, _ = tf.nn.dynamic_rnn(cell_fw, inputs=inputs, initial_state=initial_state_fw)
+    output_bw, _ = tf.nn.dynamic_rnn(cell_bw, inputs=inputs, initial_state=initial_state_bw)
+    print('Output Fw, Bw', output_fw, output_bw)
+    output_bw = tf.reverse(output_bw, axis=[1])
+    output = tf.concat([output_fw, output_bw], axis=2)
+    print('Output', output)
+    output = tf.reshape(output, [-1, FLAGS.num_units * 2])
+    print('Output Reshape', output)
+    
+    # Output Layer
+    with tf.variable_scope('outputs'):
+        w = weight([FLAGS.num_units * 2, FLAGS.category_num])
+        b = bias([FLAGS.category_num])
+        y = tf.argmax(tf.matmul(output, w) + b, axis=1)
+    print('Output Y', y)
+    
+    y_label_reshape = tf.reshape(y_label, [-1])
+    print('Y Label Reshape', y_label_reshape)
+    
+    # Prediction
+    correct_prediction = tf.equal(tf.cast(y, tf.int32), y_label_reshape)
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print('Prediction', correct_prediction, 'Accuracy', accuracy)
+    
+    # Loss
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=tf.cast(y_label_reshape, tf.float32),
+                                                            logits=tf.cast(y, tf.float32))
+    train = tf.train.AdamOptimizer(FLAGS.learning_rate).minimize(cross_entropy)
+    
+    # Iterator
+    train_iterator = get_generator(train_x, train_y, FLAGS.batch_size)
+    sess = tf.Session()
+    sess.run(train_iterator.initializer)
+    sess.run(tf.global_variables_initializer())
+    
+    # Train
+    for step in range(FLAGS.steps + 1):
+        batch_x, batch_y = sess.run(train_iterator.get_next())
+        print(batch_x.shape, batch_y.shape)
+        sess.run(train,
+                 feed_dict={x: batch_x, y_label: batch_y, batch_size: FLAGS.batch_size, keep_prob: FLAGS.keep_prob})
+        
+        # if step % FLAGS.steps_per_test == 0:
+        #     print('Test', sess.run(accuracy, feed_dict={x: batch_size: FLAGS.batch_size, }))
+        
+        
+        
+        print(sess.run(train_iterator.get_next(), feed_dict={batch_size: FLAGS.batch_size}))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='BI LSTM')
+    parser.add_argument('--batch_size', help='batch size', default=50)
+    parser.add_argument('--source_data', help='source size', default='./data/data.pkl')
+    parser.add_argument('--num_layer', help='num of layer', default=2, type=int)
+    parser.add_argument('--num_units', help='num of units', default=64, type=int)
+    parser.add_argument('--time_step', help='time steps', default=32, type=int)
+    parser.add_argument('--embedding_size', help='time steps', default=64, type=int)
+    parser.add_argument('--category_num', help='category num', default=5, type=int)
+    parser.add_argument('--learning_rate', help='learning rate', default=0.01, type=float)
+    parser.add_argument('--steps', help='steps', default=5000, type=int)
+    parser.add_argument('--steps_per_test', help='steps per test', default=1000, type=int)
+    parser.add_argument('--steps_per_valid', help='steps per valid', default=100, type=int)
+    parser.add_argument('--keep_prob', help='train keep prob dropout', default=0.5, type=float)
+    
+    FLAGS = parser.parse_args()
+    print(FLAGS)
+    main()
---
+++ pre_process.py
@@ -0,0 +1,96 @@
+import re
+from itertools import chain
+import pandas as pd
+import numpy as np
+import pickle
+
+text = open('data.txt', encoding='utf-8').read()
+sentences = re.split('[，。！？、‘’“”]/[bems]', text)
+sentences = list(filter(lambda x: x.strip(), sentences))
+sentences = list(map(lambda x: x.strip(), sentences))
+
+words, labels = [], []
+print('Start creating words and labels...')
+for sentence in sentences:
+    groups = re.findall('(.)/(.)', sentence)
+    arrays = np.asarray(groups)
+    words.append(arrays[:, 0])
+    labels.append(arrays[:, 1])
+print('Words Length', len(words), 'Labels Length', len(labels))
+print('Words Example', words[0])
+print('Labels Example', labels[0])
+
+all_words = list(chain(*words))
+print(len(all_words))
+
+print(len(all_words))
+
+# All words to Series
+all_words_sr = pd.Series(all_words)
+# Get value count, index changed to set
+all_words_counts = all_words_sr.value_counts()
+# Get words set
+all_words_set = all_words_counts.index
+# Get words ids
+all_words_ids = range(1, len(all_words_set) + 1)
+
+# Dict to transform
+word2id = pd.Series(all_words_ids, index=all_words_set)
+id2word = pd.Series(all_words_set, index=all_words_ids)
+
+# Tag set and ids
+tags_set = ['x', 's', 'b', 'm', 'e']
+tags_ids = range(len(tags_set))
+
+# Dict to transform
+tag2id = pd.Series(tags_ids, index=tags_set)
+id2tag = pd.Series(tags_set, index=tag2id)
+
+max_length = 32
+
+
+def x_transform(words):
+    ids = list(word2id[words])
+    if len(ids) >= max_length:
+        ids = ids[:max_length]
+    ids.extend([0] * (max_length - len(ids)))
+    return ids
+
+
+def y_transform(tags):
+    ids = list(tag2id[tags])
+    if len(ids) >= max_length:
+        ids = ids[:max_length]
+    ids.extend([0] * (max_length - len(ids)))
+    return ids
+
+
+print('Starting transform...')
+
+data_x = list(map(lambda x: x_transform(x), words))
+data_y = list(map(lambda y: y_transform(y), labels))
+
+print('Data X Length', len(data_x), 'Data Y Length', len(data_y))
+print('Data X Example', data_x[0])
+print('Data Y Example', data_y[0])
+
+data_x = np.asarray(data_x)
+data_y = np.asarray(data_y)
+
+from os import makedirs
+from os.path import exists, join
+
+path = 'data/'
+
+if not exists(path):
+    makedirs(path)
+
+print('Starting pickle to file...')
+with open(join(path, 'data.pkl'), 'wb') as f:
+    pickle.dump(data_x, f)
+    pickle.dump(data_y, f)
+    pickle.dump(word2id, f)
+    pickle.dump(id2word, f)
+    pickle.dump(tag2id, f)
+    pickle.dump(id2tag, f)
+print('Pickle finished')
