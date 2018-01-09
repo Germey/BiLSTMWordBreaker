ba81baf03cad238982a14bc8a0f852e969551f98
Germey <cqc@cuiqingcai.com>
2018-01-10 00:24:51 +0800

    finish iterator

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import tensorflow as tf
 import pickle
+import math
 from sklearn.model_selection import train_test_split
 
 FLAGS = None
@@ -33,25 +34,12 @@ def get_data(data_x, data_y):
     print('Data Y Example', data_y[0])
     
     train_x, test_x, train_y, test_y = train_test_split(data_x, data_y, test_size=0.2, random_state=40)
-    train_x, valid_x, train_y, valid_y = train_test_split(train_x, train_y, test_size=0.2, random_state=40)
+    train_x, dev_x, train_y, dev_y = train_test_split(train_x, train_y, test_size=0.2, random_state=40)
     
     print('Train X Shape', train_x.shape, 'Train Y Shape', train_y.shape)
-    print('Valid X Shape', valid_x.shape, 'Valid Y Shape', valid_x.shape)
+    print('Dev X Shape', dev_x.shape, 'Dev Y Shape', dev_y.shape)
     print('Test Y Shape', test_x.shape, 'Test Y Shape', test_y.shape)
-    return train_x, train_y, valid_x, valid_y, test_x, test_y
-
-
-def get_generator(data_x, data_y, batch_size):
-    """
-    Get generator of x, y data
-    :param data_x:
-    :param data_y:
-    :return: Iterator
-    """
-    dataset = tf.data.Dataset.from_tensor_slices((data_x, data_y))
-    dataset = dataset.batch(batch_size)
-    iterator = dataset.make_initializable_iterator()
-    return iterator
+    return train_x, train_y, dev_x, dev_y, test_x, test_y
 
 
 def weight(shape, stddev=0.1, mean=0):
@@ -73,15 +61,40 @@ def main():
     # Load data
     data_x, data_y, word2id, id2word, tag2id, id2tag = load_data()
     # Split data
-    train_x, train_y, valid_x, valid_y, test_x, test_y = get_data(data_x, data_y)
+    train_x, train_y, dev_x, dev_y, test_x, test_y = get_data(data_x, data_y)
+    
+    train_steps = math.ceil(train_x.shape[0] / FLAGS.train_batch_size)
+    dev_steps = math.ceil(dev_x.shape[0] / FLAGS.dev_batch_size)
+    
+    print(train_steps)
     
     vocab_size = len(word2id) + 1
     print('Vocab Size', vocab_size)
     
+    global_step = tf.Variable(0, trainable=False, name='global_step')
+    
+    # Train and dev dataset
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_x, train_y))
+    train_dataset = train_dataset.batch(FLAGS.train_batch_size)
+    
+    print(train_dataset)
+    
+    dev_dataset = tf.data.Dataset.from_tensor_slices((dev_x, dev_y))
+    dev_dataset = dev_dataset.batch(FLAGS.dev_batch_size)
+    
+    # A reinitializable iterator
+    iterator = tf.data.Iterator.from_structure(train_dataset.output_types, train_dataset.output_shapes)
+    
+    next_batch = iterator.get_next()
+    
+    train_initializer = iterator.make_initializer(train_dataset)
+    dev_initializer = iterator.make_initializer(dev_dataset)
+    
+    print(next_batch)
+    
     # Input Layer
     with tf.variable_scope('inputs'):
-        x = tf.placeholder(tf.int32, [None, FLAGS.time_step], name='x_input')
-        y_label = tf.placeholder(tf.int32, [None, FLAGS.time_step], name='y_label')
+        x, y_label = iterator.get_next()
     
     # Embedding Layer
     with tf.variable_scope('embedding'):
@@ -89,14 +102,13 @@ def main():
     inputs = tf.nn.embedding_lookup(embedding, x)
     
     # Variables
-    batch_size = tf.placeholder(tf.int32, [])
     keep_prob = tf.placeholder(tf.float32, [])
     
     # RNN Layer
     cell_fw = tf.nn.rnn_cell.MultiRNNCell([lstm_cell(FLAGS.num_units, keep_prob) for _ in range(FLAGS.num_layer)])
     cell_bw = tf.nn.rnn_cell.MultiRNNCell([lstm_cell(FLAGS.num_units, keep_prob) for _ in range(FLAGS.num_layer)])
-    initial_state_fw = cell_fw.zero_state(x.shape[0], tf.float32)
-    initial_state_bw = cell_bw.zero_state(batch_size, tf.float32)
+    initial_state_fw = cell_fw.zero_state(tf.shape(x)[0], tf.float32)
+    initial_state_bw = cell_bw.zero_state(tf.shape(x)[0], tf.float32)
     print('Inputs', inputs)
     inputs = tf.unstack(inputs, FLAGS.time_step, axis=1)
     print('Inputs unstack', inputs)
@@ -119,7 +131,7 @@ def main():
         y = tf.matmul(output, w) + b
     print('Output Y', y)
     
-    y_label_reshape = tf.reshape(y_label, [-1])
+    y_label_reshape = tf.cast(tf.reshape(y_label, [-1]), tf.int32)
     print('Y Label Reshape', y_label_reshape)
     
     # Prediction
@@ -130,44 +142,36 @@ def main():
     # Loss
     cross_entropy = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_label_reshape,
                                                                                   logits=tf.cast(y, tf.float32)))
-    train = tf.train.AdamOptimizer(FLAGS.learning_rate).minimize(cross_entropy)
+    train = tf.train.AdamOptimizer(FLAGS.learning_rate).minimize(cross_entropy, global_step=global_step)
     
     # Iterator
-    train_iterator = get_generator(train_x, train_y, FLAGS.batch_size)
     sess = tf.Session()
-    sess.run(train_iterator.initializer)
     sess.run(tf.global_variables_initializer())
     
     # Saver
-    saver = tf.train.Saver()
-    
-    # Train
-    for step in range(0, FLAGS.steps + 1):
-        batch_x, batch_y = sess.run(train_iterator.get_next())
-        sess.run(train,
-                 feed_dict={x: batch_x, y_label: batch_y, batch_size: FLAGS.batch_size, keep_prob: FLAGS.keep_prob})
+    # saver = tf.train.Saver()
+    for epoch in range(FLAGS.epoch_num):
+        tf.train.global_step(sess, global_step_tensor=global_step)
+        # Train
+        sess.run(train_initializer)
+        for step in range(int(train_steps)):
+            sess.run(train,
+                     feed_dict={keep_prob: FLAGS.keep_prob})
+            loss, acc, gstep = sess.run([cross_entropy, accuracy, global_step],
+                                        feed_dict={keep_prob: 1})
+            print('Global Step', gstep, 'Step', step, 'Train Loss', loss, 'Accuracy', acc)
         
-        loss, acc = sess.run([cross_entropy, accuracy],
-                             feed_dict={x: batch_x, y_label: batch_y, batch_size: FLAGS.batch_size,
-                                        keep_prob: FLAGS.keep_prob})
-        print('Train Loss ', loss, 'Accuracy', acc)
-        
-        print(test_x.shape, test_y.shape, valid_x.shape, valid_y.shape)
-        if step % FLAGS.steps_per_valid == 0:
-            print('Valid Accuracy',
-                  sess.run(accuracy, feed_dict={x: valid_x, y_label: valid_y, batch_size: valid_x.shape[0],
-                                                keep_prob: 1}))
-        if step % FLAGS.steps_per_test == 0:
-            print('Test Accuracy',
-                  sess.run(accuracy, feed_dict={x: test_x, y_label: test_y, batch_size: test_x.shape[0],
-                                                keep_prob: 1}))
-        # if step % FLAGS.steps_per_save == 0:
-        #     save_path = 'ckpt/bi-lstm-{}'.format()
-        #     saver.restore(sess=sess, save_path='d')
+        if epoch % FLAGS.epochs_per_dev == 0:
+            # Dev
+            sess.run(dev_initializer)
+            for step in range(int(dev_steps)):
+                print('Dev Accuracy', sess.run(accuracy, feed_dict={keep_prob: 1}), 'Step', step)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='BI LSTM')
-    parser.add_argument('--batch_size', help='batch size', default=50)
+    parser.add_argument('--train_batch_size', help='train batch size', default=50)
+    parser.add_argument('--dev_batch_size', help='dev batch size', default=50)
     parser.add_argument('--source_data', help='source size', default='./data/data.pkl')
     parser.add_argument('--num_layer', help='num of layer', default=2, type=int)
     parser.add_argument('--num_units', help='num of units', default=64, type=int)
@@ -176,8 +180,8 @@ if __name__ == '__main__':
     parser.add_argument('--category_num', help='category num', default=5, type=int)
     parser.add_argument('--learning_rate', help='learning rate', default=0.01, type=float)
     parser.add_argument('--epoch_num', help='num of epoch', default=1000, type=int)
-    parser.add_argument('--steps_per_test', help='steps per test', default=1000, type=int)
-    parser.add_argument('--steps_per_valid', help='steps per valid', default=100, type=int)
+    parser.add_argument('--epochs_per_test', help='steps per test', default=100, type=int)
+    parser.add_argument('--epochs_per_dev', help='steps per dev', default=2, type=int)
     parser.add_argument('--steps_per_save', help='steps per save', default=2000, type=int)
     parser.add_argument('--keep_prob', help='train keep prob dropout', default=0.5, type=float)
     
